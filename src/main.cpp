f4cb1f5c81ea1e84209f672524bb5e8d4caa6670
Daria Lekhtereva <darialekhtereva@gmail.com>
2025-02-18 20:29:34 +0300

    Uodate main file

--- src/main.cpp
+++ src/main.cpp
@@ -6,4 +6,6 @@ int sub(int c, int d);
 int main(){
     std::cout << add(2, 3) << std::endl;
     std::cout << sub(10, 2) << std::endl;
+    std::cout << mul(2, 5) << std::endl;
+    std::cout << div(12, 6) << std::endl;
 }
